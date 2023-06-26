1b004a9dcc32d35fee455aba9891465e2f96c44b
Ilya Lobchikov <ilya11141890@gmail.com>
2023-06-27 01:28:17 +0300

    Deleted WeatherForecastController; Added PingController.

---
+++ PostBoard.Api/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PostBoard.Api.Controllers;
+
+[Route("api/ping")]
+[ApiController]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok();
+    }
+}
