3ed00da7aad9af2738c91fc3c7c00a0bc83d03af
Ilya Lobchikov <ilya11141890@gmail.com>
2023-06-27 23:25:55 +0300

    Added PostController; Added endpoint GetAll; Added endpoint Create.

---
+++ PostBoard.Api/Controllers/PostController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostBoard.Api.Data;
+
+namespace PostBoard.Api.Controllers;
+
+[Route("api/posts")]
+[ApiController]
+public class PostController : ControllerBase
+{
+    private static List<Post> Posts = new List<Post>();
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(Posts);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] Post input)
+    {
+        if (Posts.Count == 0)
+        {
+            input.Id = 1;
+        }
+        else
+        {
+            var maxId = Posts.Max(post => post.Id);
+            input.Id = maxId + 1;
+        }
+            
+        Posts.Add(input);
+
+        return Ok(input);
+    }
+}
