5505cb11e823d849201f2e9458eb3d53375d9513
Ilia <Ilia@DESKTOP-2940G74>
2023-09-10 21:19:06 +0300

    Added  UserSecrets.

--- PostBoard.Api/Program.cs
+++ PostBoard.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PostBoard.Api;
@@ -7,8 +8,12 @@ using PostBoard.Api;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<PostBoardContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5432;Database=postdb;UserName=postgres;Password=rootroot"));
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Services.AddDbContext<PostBoardContext>((serviceProvider, options) =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("PostBoardContext");
+    options.UseNpgsql(connectionString);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
